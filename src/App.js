5bca808e7219680df084f51ad5387e1cdbcf0f4a
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-11-23 08:10:02 +1100

    Added a posts component to return all our posts from json placeholder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Posts from './components/Posts';
 
 function App() {
+  
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+     const fetchPosts = async () => {
+       setLoading(true);
+       const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+       setPosts(res.data);
+       setLoading(false);
+     }
+
+     fetchPosts();
+  }, []);
+ 
   return (
-    <div className="container">
-      <h1>My App</h1>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Posts posts={posts} loading={loading}/>
     </div>
   );
 }
